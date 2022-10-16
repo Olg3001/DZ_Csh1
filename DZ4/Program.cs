5407c03ec8fa88f64285cd99499e0e308cc9bf53
Olga <olg3001@yandex.ru>
2022-10-16 20:07:34 +0300

    Changed DZ4

--- DZ4/Program.cs
+++ DZ4/Program.cs
@@ -1,19 +1,17 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
-// какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 Console.WriteLine("Введите число а: ");
 int number_1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число b: ");
 int number_2 = int.Parse(Console.ReadLine()!);
-
-if(number_1 > number_2)
-{
-    Console.WriteLine("Из двух чисел max = " + number_1);
-}
-else 
-{
-    Console.WriteLine("Из двух чисел max = " + number_2);
-}
+Console.WriteLine("Введите число c: ");
+int number_3 = int.Parse(Console.ReadLine()!);
+int max = number_1;
+if(number_1 > max) max = number_1;
+if(number_2 > max) max = number_2;
+if(number_3 > max) max = number_3;
+Console.Write("max = " + max);
