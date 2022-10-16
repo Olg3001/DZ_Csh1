8b36154e8c800fc00340fb8f884b25775a962041
Olga <olg3001@yandex.ru>
2022-10-16 19:46:21 +0300

    DZ8

---
+++ DZ8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine()!);
+int a = 2;
+if(number > 1) // Было: + Console.Write(a + " "); работало при n-1 в while - громоздко
+    while(number >= a)
+        {
+            Console.Write(a + " ");
+            a = a + 2;
+        }
+if(number <= 1) Console.WriteLine("Wrong input");
